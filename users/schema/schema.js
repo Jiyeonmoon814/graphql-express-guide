fd68ebd8327ddee9f6cd0abf221dcfa9aff13700
jiyeonmoon <jiyeonmoon814@gmail.com>
2023-05-24 15:20:38 +0100

    Add a company type and resolve it in the user type

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -2,12 +2,34 @@ const graphql = require("graphql");
 const { GraphQLString, GraphQLInt, GraphQLSchema, GraphQLObjectType } = graphql;
 const axios = require("axios");
 
+// user model ---------------------- User Type
+// propertyName : Type
+// companyId : Id <-- different --> company : Company
+// resolve required!
+
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  },
+});
+
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then((res) => res.data);
+      },
+    },
   },
 });
 
